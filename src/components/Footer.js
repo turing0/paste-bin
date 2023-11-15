de35c6d22462116fec1bb0da33dba45cf132f5e4
turing0 <turing175@gmail.com>
2023-11-15 19:39:33 +0800

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -35,7 +35,7 @@ const Footer = () => {
         <div className="mt-10 flex justify-center space-x-10">
           {navigation.social.map((item) => (
             <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500 dark:text-gray-300 dark:hover:text-white">
-              <span className="sr-only">{item.name}</span>
+              {/* <span className="sr-only">{item.name}</span> */}
               <item.icon className="h-6 w-6" aria-hidden="true" />
             </a>
           ))}
