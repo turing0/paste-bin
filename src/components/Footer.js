e494c02029bddd8fd8c0aa157ea700f8931a22ed
turing0 <turing175@gmail.com>
2023-11-15 19:58:09 +0800

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -22,7 +22,7 @@ const navigation = {
 const Footer = () => {
   return (
     <footer className="bg-white dark:bg-gray-800">
-      <div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
+      <div className="mx-auto max-w-7xl overflow-hidden px-6 py-10 sm:py-12 lg:px-8">
         {/* <nav className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12" aria-label="Footer">
           {navigation.main.map((item) => (
             <div key={item.name} className="pb-6">
@@ -32,7 +32,7 @@ const Footer = () => {
             </div>
           ))}
         </nav> */}
-        <div className="mt-10 flex justify-center space-x-10">
+        <div className="flex justify-center space-x-10">
           {navigation.social.map((item) => (
             <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500 dark:text-gray-300 dark:hover:text-white">
               <span className="sr-only">{item.name}</span>
@@ -40,9 +40,9 @@ const Footer = () => {
             </a>
           ))}
         </div>
-        <p className="mt-10 text-center text-xs leading-5 text-gray-500">
+        {/* <p className="mt-10 text-center text-xs leading-5 text-gray-500">
           Paste.laphel.com
-        </p>
+        </p> */}
       </div>
     </footer>
   )
