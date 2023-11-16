814af47100cf99b37aca27301572dea75a2f8caa
turing0 <turing175@gmail.com>
2023-11-16 20:00:46 +0800

    update index

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import Faq from '@/components/Faq'
 import Hero from '@/components/Hero'
-import Steps from '@/components/Steps'
+// import Steps from '@/components/Steps'
 import PasteArea from '@/components/form/PasteArea'
 
 export default function Home() {
@@ -9,7 +9,7 @@ export default function Home() {
       <div>
         <Hero />
         {/* <div className="container mx-auto px-4 lg:w-2/4 my-24"> */}
-        <div className="container mx-auto px-4 lg:w-2/4 mt-12 mb-24">
+        <div className="container mx-auto px-4 lg:w-3/4 mt-12 mb-24">
           {/* <Steps /> */}
           <PasteArea />
         </div>
