a818ab3a1d5bdae4ca34458b7db56f6830736796
turing0 <turing175@gmail.com>
2023-11-16 13:19:25 +0800

    update page

--- src/app/snippet/[id]/page.js
+++ src/app/snippet/[id]/page.js
@@ -23,7 +23,7 @@ const Snippet = async ({ params }) => {
   }
 
   return (
-    <div className="container mx-auto px-4 lg:w-3/4 my-12">
+    <div className="container mx-auto px-4 lg:w-3/4 my-6">
       {data.expired && (
         <div>
           <h1 className="text-3xl font-bold text-center text-gradient">Snippet has expired</h1>
