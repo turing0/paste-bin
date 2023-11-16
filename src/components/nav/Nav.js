34a8681e287a66a32c1fdbad7d8434c56bd7a818
turing0 <turing175@gmail.com>
2023-11-16 20:05:34 +0800

    update index

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -34,7 +34,7 @@ export default function Nav() {
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-            <div className="flex h-16 justify-between">
+            <div className="flex h-14 justify-between">
               <div className="flex">
                 <div className="flex flex-shrink-0 items-center">
                   <Link className="text-gradient" href="/">
